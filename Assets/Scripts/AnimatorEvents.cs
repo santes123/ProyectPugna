c565cf801a5a7295770f4ee954846858776015c3
Mauricio <mau.movilla@gmail.com>
2023-06-24 16:41:16 -0400

    hitfixes y audio en personajes

--- Assets/Scripts/AnimatorEvents.cs
+++ Assets/Scripts/AnimatorEvents.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class AnimatorEvents : MonoBehaviour
 {
+    public AudioSource steps;
+    public AudioSource gotHit;
+    public AudioSource dead;
     void FootR() {
-    
+        if(steps != null) {
+            steps.Play();
+        }
     }
 
     void FootL() {
-    
+        if(steps != null) {
+            steps.Play();
+        }
     }
 
     void Hit() {
         GetComponentInParent<IDamager>().Attack();
+    }
+
+    void GetHit() {
+        if(gotHit != null) {
+            gotHit.Play();
+        }
+    }
+
+    void Death() {
+        if(dead != null) {
+            dead.Play();
+        }
     }
 }
