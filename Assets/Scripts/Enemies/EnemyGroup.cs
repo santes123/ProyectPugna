8d8d469338dc6acea5ad1b43f8bc0878418e97d4
Mauricio <mau.movilla@gmail.com>
2023-07-08 13:36:51 -0400

    Enemy Groups

---
+++ Assets/Scripts/Enemies/EnemyGroup.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroup : MonoBehaviour
+{
+    public EnemyBase[] enemy;
+    public GameObject ItemDropped;
+
+
+    private void Start() {
+        
+    }
+
+}
